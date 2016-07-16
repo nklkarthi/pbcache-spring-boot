f4e11380cd3fb0bc182e942a0afedef8aa456456
Karthikeyan Balasubramaniam <nklkarthi@gmail.com>
2016-07-16 19:00:11 +0800

    Hazelcast Spring boot auto config with spring profile

--- src/main/java/com/csg/dpb/hazelcast/PbcacheApplication.java
+++ src/main/java/com/csg/dpb/hazelcast/PbcacheApplication.java
@@ -1,14 +1,17 @@
 package com.csg.dpb.hazelcast;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PbcacheApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(PbcacheApplication.class, args);
+        new SpringApplicationBuilder()
+                .sources(PbcacheApplication.class)
+                .run(args);
     }
 
 }
